81f9c6f5148d816cc49973d37fb81d0b65a453c5
hkhv <746392783@qq.com>
2018-07-12 18:27:30 +0800

    Update transaction-receive

--- app/src/main/java/com/iwallic/app/pages/transaction/TransactionReceiveActivity.kt
+++ app/src/main/java/com/iwallic/app/pages/transaction/TransactionReceiveActivity.kt
@@ -1,6 +1,7 @@
 package com.iwallic.app.pages.transaction
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.iwallic.app.R
 import com.iwallic.app.base.BaseActivity
 
@@ -8,6 +9,10 @@ class TransactionReceiveActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         setContentView(R.layout.activity_transaction_receive)
+
     }
 }
