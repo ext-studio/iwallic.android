a9e7eba46eda89d69ad3e81671ee3eef96d303af
hkhv <746392783@qq.com>
2018-07-13 16:05:17 +0800

    Update transfer

--- app/src/main/java/com/iwallic/app/pages/transaction/TransactionTransferActivity.kt
+++ app/src/main/java/com/iwallic/app/pages/transaction/TransactionTransferActivity.kt
@@ -10,10 +10,17 @@ import com.iwallic.app.R
 import com.iwallic.app.utils.DialogUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.iwallic.app.models.TransactionModel
+import com.iwallic.app.models.UtxoModel
+import com.iwallic.app.models.addrassets
 import com.iwallic.app.states.AssetState
 import com.iwallic.app.utils.HttpClient
 import com.iwallic.app.utils.WalletUtils
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
 import org.w3c.dom.Text
 
 class TransactionTransferActivity : BaseActivity() {
@@ -26,6 +33,9 @@ class TransactionTransferActivity : BaseActivity() {
     private var balanceTV: TextView ?= null
     private var submitBT: Button ?= null
     private var submitPB: ProgressBar ?= null
+    private var tipTV: TextView ?= null
+    private val gson = Gson()
+    private var wif: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_transfer)
@@ -44,6 +54,7 @@ class TransactionTransferActivity : BaseActivity() {
         balanceTV = findViewById(R.id.transaction_transfer_balance)
         submitBT = findViewById(R.id.transaction_transfer_btn_submit)
         submitPB = findViewById(R.id.transaction_transfer_load_submit)
+        tipTV = findViewById(R.id.transaction_transfer_error)
     }
 
     private fun initClick() {
@@ -72,10 +83,28 @@ class TransactionTransferActivity : BaseActivity() {
         }
 
         submitBT!!.setOnClickListener {
-            it.visibility = View.GONE
-            submitPB?.visibility = View.VISIBLE
-            target = targetTV?.text.toString()
-            amount = amountTV?.text.toString().toFloat()
+            target = this.targetTV?.text.toString()
+            if(amountTV?.text.toString() != "") {
+                amount = amountTV?.text.toString().toFloat()
+            }
+            if(chooseAssetId === "") {
+                this.tipTV?.text = resources.getText(R.string.transaction_transfer_tips_noChooseAsset_error)
+                this.tipTV?.visibility = View.VISIBLE
+            } else if(target === "") {
+                this.tipTV?.text = resources.getText(R.string.transaction_transfer_tips_target_error)
+                this.tipTV?.visibility = View.VISIBLE
+            } else if(amount <= 0) {
+                this.tipTV?.text = resources.getText(R.string.transaction_transfer_tips_amount_error)
+                this.tipTV?.visibility = View.VISIBLE
+            } else {
+                target = targetTV?.text.toString()
+                amount = amountTV?.text.toString().toFloat()
+                this.tipTV?.visibility = View.INVISIBLE
+                it.visibility = View.GONE
+                submitPB?.visibility = View.VISIBLE
+                this.resolveVerify(address, target, chooseAssetId, amount)
+            }
+
         }
     }
 
@@ -90,4 +119,54 @@ class TransactionTransferActivity : BaseActivity() {
         }
     }
 
+    private fun send(from: String, to: String, assetId: String, amount: Float) {
+        var asset: String = ""
+        if (assetId.length === 64) {
+            asset = "0x $assetId"
+        }else if (assetId.length === 42) {
+            asset = assetId.substring(2, assetId.length)
+        } else {
+            asset = assetId
+        }
+        Log.i("asset",asset)
+        if(WalletUtils.check(assetId, "asset")) {
+            HttpClient.post("getutxoes", listOf(from, asset), fun(res) {
+                Log.i("UTXO", res.toString())
+                val data = gson.fromJson<ArrayList<UtxoModel>>(res, object: TypeToken<ArrayList<UtxoModel>>() {}.type)
+                Log.i("UTXO",data.toString())
+                val newTx = TransactionModel.forAsset(data, from, to, amount, asset)
+                newTx?.sign(wif)
+                HttpClient.post("sendv4rawtransaction", listOf(newTx!!.serialize(true)), fun(res) {
+                    Log.i("transaction",res.toString())
+                }, fun(err) {
+
+                })
+            }, fun(err) {
+
+            })
+        } else  {
+
+        }
+    }
+
+    private fun resolveVerify(from: String, to: String, assetId: String, amount: Float) {
+        DialogUtils.Password(this).subscribe {
+            if (it.isEmpty()) {
+                return@subscribe
+            }
+            DialogUtils.load(this) { load ->
+                launch {
+                    wif = WalletUtils.verify(baseContext, it)
+                    load.dismiss()
+                    withContext(UI) {
+                        if (wif.isEmpty()) {
+                            Toast.makeText(baseContext, "密码错误", Toast.LENGTH_SHORT).show()
+                        } else {
+                            send(from, to, assetId, amount)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
