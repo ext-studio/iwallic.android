ef973ca48c91f7002330a4f1c09725f97b0a0d9a
haiwulei <1021391538@qq.com>
2018-07-17 21:43:58 +0800

    'transaction-detail'

--- app/src/main/java/com/iwallic/app/pages/transaction/TransactionDetailActivity.kt
+++ app/src/main/java/com/iwallic/app/pages/transaction/TransactionDetailActivity.kt
@@ -1,13 +1,37 @@
 package com.iwallic.app.pages.transaction
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.iwallic.app.base.BaseActivity
 import com.iwallic.app.R
+import com.iwallic.app.utils.HttpClient
 
 class TransactionDetailActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_detail)
+
+        var txid = this.intent.extras.get("txid")
+        findViewById<TextView>(R.id.transaction_detail_txid).setText("${txid}")
+
+        HttpClient.post("gettransferbytxid", listOf("${txid}"), fun (res) {
+            Log.e("【交易详情】", "${res}")
+        }, fun(err) {
+            Log.e("【接收交易失败】", "${err}")
+        })
+
+        HttpClient.post("getnep5transferbytxid", listOf("${txid}"), fun (res) {
+            Log.e("【交易详情】", "${res}")
+        }, fun(err) {
+            Log.e("【接收交易失败】", "${err}")
+        })
+
+        val backLL = findViewById<LinearLayout>(R.id.transaction_detail__back)
+        backLL.setOnClickListener {
+            finish()
+        }
     }
 }
